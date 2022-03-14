54ae8a48289bf385a70a6292a0c53cd755b8533c
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-03-14 18:13:06 +0500

    add js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+const hamburger = document.querySelector('.header__mobile-btn');
+const menu = document.querySelector('.header__desktop');
+const closeBtn = document.querySelector('.header__close--button');
+const curtain = document.querySelector('.curtain');
+
+hamburger.addEventListener('click', (e) => {
+    menu.classList.add('header__desktop--on');
+    curtain.classList.add('curtain--on')
+})
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('header__desktop--on')
+    curtain.classList.remove('curtain--on')
+})
